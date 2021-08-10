3b353cf42211e94f73a1d93d4fae8e1bf2fa9604
oleg15-cloud <karaivanoleg093@gmail.com>
2021-08-10 17:33:45 +0300

    Change default url

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ def pytest_addoption(parser):
     parser.addoption("--maximized", action="store_true", help="Maximize browser windows")
     parser.addoption("--headless", action="store_true", help="Run headless")
     parser.addoption("--browser", action="store", default="chrome", choices=["chrome", "firefox", "opera"])
-    parser.addoption("--url", action="store", default="http://192.168.0.107:8081/")
+    parser.addoption("--url", action="store", default="https://demo.opencart.com/")
 
 
 @pytest.fixture(scope="session")
